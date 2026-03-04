1e10e03533bcead40c0514167edd046997f7a16d
Ilya Nazarchuk <uriyvich@gmail.com>
2026-03-03 20:52:21 -0800

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+import os
+from openai import OpenAI
+import streamlit as st
+
+st.title("DeepSleek Chat")
+client = OpenAI(api_key=os.environ.get('DEEPSEEK_API_KEY'), base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=True
+)
+
+for chunk in response:
+    print(chunk.choices[0].delta.content, end="")
